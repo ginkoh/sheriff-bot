0b0a37c9e2ff87a067831a4ad84daba20dec4612
Gian <gianszabernardo@gmail.com>
2019-05-12 00:07:30 -0300

    Remove extra line

--- src/modules/messages.js
+++ src/modules/messages.js
@@ -21,7 +21,6 @@ const deleteAllMessages = async (channel, limit = 100) => {
     } else {
         // Set the custom error message.
         const errorMessage = "Cannot delete more than 100 messages.";
-
         // Send the error message to the channel.
         channel.sendMessage(errorMessage);
         // Throw a new error.
